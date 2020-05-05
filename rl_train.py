37bab619dcfb4ac67395814258e63c0acb31cd3f
Miguel Araújo <mdaraujo@ua.pt>
2020-05-05 23:09:35 +0100

    Saving training parameters in a JSON file

--- rl_train.py
+++ rl_train.py
@@ -1,6 +1,8 @@
 import os
 import argparse
+import json
 from datetime import datetime
+from collections import OrderedDict
 
 from stable_baselines.bench import Monitor
 
@@ -22,7 +24,7 @@ if __name__ == "__main__":
                         help="Number of evaluation episodes. (default: 30)")
     parser.add_argument("--map", help="path to the map bmp", default="data/map1.bmp")
     parser.add_argument("--ghosts", help="Number of ghosts", type=int, default=1)
-    parser.add_argument("--level", help="difficulty level of ghosts", choices=['0', '1', '2', '3'], default='1')
+    parser.add_argument("--level", help="difficulty level of ghosts", choices=[0, 1, 2, 3], default=1)
     parser.add_argument("--lives", help="Number of lives", type=int, default=3)
     parser.add_argument("--timeout", help="Timeout after this amount of steps", type=int, default=3000)
     args = parser.parse_args()
@@ -42,7 +44,22 @@ if __name__ == "__main__":
 
     print("\nLog dir:", log_dir)
 
-    env = PacmanEnv(alg_name, args.map, args.ghosts, int(args.level), args.lives, args.timeout)
+    params = OrderedDict()
+    params['alg'] = alg_name
+    params['timesteps'] = args.timesteps
+    params['eval_freq'] = eval_freq
+    params['eval_episodes'] = args.eval_episodes
+    params['map'] = args.map
+    params['ghosts'] = args.ghosts
+    params['level'] = args.level
+    params['lives'] = args.lives
+    params['timeout'] = args.timeout
+    params['datetime'] = now.replace(microsecond=0).isoformat()
+
+    with open(os.path.join(log_dir, "params.json"), "w") as f:
+        json.dump(params, f, indent=4)
+
+    env = PacmanEnv(alg_name, args.map, args.ghosts, args.level, args.lives, args.timeout)
     env = Monitor(env, filename=log_dir)
 
     filter_tf_warnings()
