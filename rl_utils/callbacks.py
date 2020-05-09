8ef2ef463c7466d5d478f2481451090db60ff7f7
Miguel Araújo <mdaraujo@ua.pt>
2020-05-09 01:50:53 +0100

    Evaluating PPO on training end

--- rl_utils/callbacks.py
+++ rl_utils/callbacks.py
@@ -5,22 +5,25 @@ from collections import OrderedDict
 
 import numpy as np
 from tqdm.auto import tqdm
-from stable_baselines.common.callbacks import EvalCallback
+from stable_baselines.common.callbacks import BaseCallback
+from stable_baselines.common.evaluation import evaluate_policy
 from stable_baselines.results_plotter import load_results, ts2xy
 
 from rl_utils.utils import get_formated_time, write_rows, EVAL_HEADER, plot_line, plot_error_bar
 
 
-class PlotEvalSaveCallback(EvalCallback):
+class PlotEvalSaveCallback(BaseCallback):
     """
     Callback for evaluating the agent during training and saving the best and the last model.
     """
 
     def __init__(self, eval_env, n_eval_episodes, eval_freq, log_dir, deterministic):
-        super().__init__(eval_env, None, n_eval_episodes, eval_freq,
-                         log_dir, log_dir, deterministic,
-                         render=False, verbose=0)
-
+        super().__init__(verbose=0)
+        self.eval_env = eval_env
+        self.n_eval_episodes = n_eval_episodes
+        self.eval_freq = eval_freq
+        self.best_mean_reward = -np.inf
+        self.deterministic = deterministic
         self.log_dir = log_dir
         self.pbar = None
         self.start_time = None
@@ -43,50 +46,64 @@ class PlotEvalSaveCallback(EvalCallback):
         self.pbar.update(0)
 
         if self.eval_freq > 0 and self.n_calls % self.eval_freq == 0:
-            eval_start_time = time.time()
-            super()._on_step()
-            eval_elapsed_time = get_formated_time(time.time() - eval_start_time)
-
-            episode_rewards = self.evaluations_results[-1]
-            episode_lengths = self.evaluations_length[-1]
-
-            mean_reward, std_reward = np.mean(episode_rewards), np.std(episode_rewards)
-            mean_ep_length, std_ep_length = np.mean(episode_lengths), np.std(episode_lengths)
-
-            self.train_steps.append(self.num_timesteps)
-            self.mean_rewards.append(mean_reward)
-            self.std_rewards.append(std_reward)
-            self.max_rewards.append(max(episode_rewards)[0])
-            self.min_rewards.append(min(episode_rewards)[0])
-            self.mean_ep_lengths.append(mean_ep_length)
-            self.std_ep_lengths.append(std_ep_length)
-            self.evals_elapsed_time.append(eval_elapsed_time)
-
-            eval_episodes = [self.n_eval_episodes for _ in range(len(self.mean_rewards))]
-
-            rows = zip(self.train_steps, self.mean_rewards, self.std_rewards,
-                       self.max_rewards, self.min_rewards,
-                       self.mean_ep_lengths, self.std_ep_lengths,
-                       self.evals_elapsed_time, eval_episodes)
-
-            write_rows(os.path.join(self.log_dir, 'evaluations.csv'),
-                       rows, EVAL_HEADER)
-
-            plot_error_bar(self.train_steps, self.mean_rewards, self.std_rewards,
-                           'Evaluation Mean Episode Rewards', 'Timestep', 'Mean Episode Reward',
-                           os.path.join(self.log_dir, 'eval_rewards.png'),
-                           self.min_rewards, self.max_rewards)
-
-            plot_error_bar(self.train_steps, self.mean_ep_lengths, self.std_ep_lengths,
-                           'Evaluation Mean Episode Lengths', 'Timestep', 'Mean Episode Length',
-                           os.path.join(self.log_dir, 'eval_lengths.png'))
-
-            x, y = ts2xy(load_results(self.log_dir), 'timesteps')
-
-            plot_line(x, y, 'Training Rewards | Total Episodes: {}'.format(len(y)), 'Timestep', 'Episode Reward',
-                      os.path.join(self.log_dir, 'train_rewards.png'))
+            self.eval_save_plot()
 
         return True
+
+    def _on_training_end(self):
+        # PPO may end before total_timesteps
+        if self.num_timesteps < self.locals['total_timesteps']:
+            self.eval_save_plot()
+
+    def eval_save_plot(self):
+        eval_start_time = time.time()
+
+        episode_rewards, episode_lengths = evaluate_policy(self.model, self.eval_env,
+                                                           n_eval_episodes=self.n_eval_episodes,
+                                                           render=False,
+                                                           deterministic=self.deterministic,
+                                                           return_episode_rewards=True)
+
+        eval_elapsed_time = get_formated_time(time.time() - eval_start_time)
+
+        mean_reward, std_reward = np.mean(episode_rewards), np.std(episode_rewards)
+        mean_ep_length, std_ep_length = np.mean(episode_lengths), np.std(episode_lengths)
+
+        if mean_reward > self.best_mean_reward:
+            self.model.save(os.path.join(self.log_dir, 'best_model'))
+            self.best_mean_reward = mean_reward
+
+        self.train_steps.append(self.num_timesteps)
+        self.mean_rewards.append(mean_reward)
+        self.std_rewards.append(std_reward)
+        self.max_rewards.append(max(episode_rewards))
+        self.min_rewards.append(min(episode_rewards))
+        self.mean_ep_lengths.append(mean_ep_length)
+        self.std_ep_lengths.append(std_ep_length)
+        self.evals_elapsed_time.append(eval_elapsed_time)
+
+        eval_episodes = [self.n_eval_episodes for _ in range(len(self.mean_rewards))]
+
+        rows = zip(self.train_steps, self.mean_rewards, self.std_rewards,
+                   self.max_rewards, self.min_rewards,
+                   self.mean_ep_lengths, self.std_ep_lengths,
+                   self.evals_elapsed_time, eval_episodes)
+
+        write_rows(os.path.join(self.log_dir, 'evaluations.csv'), rows, EVAL_HEADER)
+
+        plot_error_bar(self.train_steps, self.mean_rewards, self.std_rewards,
+                       'Evaluation Mean Episode Rewards', 'Timestep', 'Mean Episode Reward',
+                       os.path.join(self.log_dir, 'eval_rewards.png'),
+                       self.min_rewards, self.max_rewards)
+
+        plot_error_bar(self.train_steps, self.mean_ep_lengths, self.std_ep_lengths,
+                       'Evaluation Mean Episode Lengths', 'Timestep', 'Mean Episode Length',
+                       os.path.join(self.log_dir, 'eval_lengths.png'))
+
+        x, y = ts2xy(load_results(self.log_dir), 'timesteps')
+
+        plot_line(x, y, 'Training Rewards | Total Episodes: {}'.format(len(y)), 'Timestep', 'Episode Reward',
+                  os.path.join(self.log_dir, 'train_rewards.png'))
 
     def __enter__(self):
         return self
