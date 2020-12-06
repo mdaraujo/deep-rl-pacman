97307e2854c5565256ae8848d9a709b29632881c
Miguel Araújo <mdaraujo@ua.pt>
2020-12-06 16:27:38 +0000

    Added agent name in all plots

--- plot_from_file.py
+++ plot_from_file.py
@@ -51,9 +51,10 @@ if __name__ == "__main__":
     # Train returns
     x, train_returns = ts2xy(train_results, 'timesteps')
 
-    plot_line(x, train_returns, 'Training Episodes Return | Total Episodes: {}'.format(len(train_returns)),
-              'Training Step', 'Episode Return',
-              os.path.join(args.logdir, 'train_returns_new.png'))
+    plot_line(x, train_returns, '{} Training Episodes Return | Total Episodes: {}'.format(
+        agent_name, len(train_returns)),
+        'Training Step', 'Episode Return',
+        os.path.join(args.logdir, 'train_returns_new.png'))
 
     moving_n = 100
     if len(train_returns) < moving_n * 2:
@@ -62,7 +63,8 @@ if __name__ == "__main__":
     moving_returns = moving_average(train_returns, n=moving_n)
 
     plot_line(x[moving_n-1:], moving_returns,
-              'Training Episodes Return Moving Average | Total Episodes: {}'.format(len(train_returns)),
+              '{} Training Episodes Return Moving Average | Total Episodes: {}'.format(
+                  agent_name, len(train_returns)),
               'Training Step', '{} Last Episodes Average Return'.format(moving_n),
               os.path.join(args.logdir, 'train_returns_MM_new.png'))
 
@@ -70,14 +72,16 @@ if __name__ == "__main__":
     if 'score' in train_results:
         train_scores = train_results['score'].tolist()
 
-        plot_line(x, train_scores, 'Training Episodes Score | Total Episodes: {}'.format(len(train_scores)),
-                  'Training Step', 'Episode Score',
-                  os.path.join(args.logdir, 'train_scores_new.png'))
+        plot_line(x, train_scores, '{} Training Episodes Score | Total Episodes: {}'.format(
+            agent_name, len(train_scores)),
+            'Training Step', 'Episode Score',
+            os.path.join(args.logdir, 'train_scores_new.png'))
 
         moving_scores = moving_average(train_scores, n=moving_n)
 
         plot_line(x[moving_n-1:], moving_scores,
-                  'Training Episodes Score Moving Average | Total Episodes: {}'.format(len(train_scores)),
+                  '{} Training Episodes Score Moving Average | Total Episodes: {}'.format(
+                      agent_name, len(train_scores)),
                   'Training Step', '{} Last Episodes Average Score'.format(moving_n),
                   os.path.join(args.logdir, 'train_scores_MM_new.png'))
 
@@ -85,13 +89,15 @@ if __name__ == "__main__":
     if 'ghosts' in train_results:
         train_ghosts = train_results['ghosts'].tolist()
 
-        plot_line(x, train_ghosts, 'Training Episodes N Ghosts | Total Episodes: {}'.format(len(train_ghosts)),
-                  'Training Step', 'Episode N Ghosts',
-                  os.path.join(args.logdir, 'train_ghosts_new.png'))
+        plot_line(x, train_ghosts, '{} Training Episodes N Ghosts | Total Episodes: {}'.format(
+            agent_name, len(train_ghosts)),
+            'Training Step', 'Episode N Ghosts',
+            os.path.join(args.logdir, 'train_ghosts_new.png'))
 
         moving_ghosts = moving_average(train_ghosts, n=moving_n)
 
         plot_line(x[moving_n-1:], moving_ghosts,
-                  'Training Episodes Ghosts Moving Average | Total Episodes: {}'.format(len(train_ghosts)),
+                  '{} Training Episodes Ghosts Moving Average | Total Episodes: {}'.format(
+                      agent_name, len(train_ghosts)),
                   'Training Step', '{} Last Episodes Average Ghosts'.format(moving_n),
                   os.path.join(args.logdir, 'train_ghosts_MM_new.png'))
